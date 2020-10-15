271b190c7bb9658d9515dd58941157a56427176f
Pauliina Raitaniemi <pr222ja@student.lnu.se>
2020-10-15 10:43:21 +0200

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,12 @@
+/**
+ * Starting point of application.
+ *
+ * @author Pauliina Raitaniemi <pr222ja@student.lnu.se>
+ * @version 1.0.0
+ */
+
+import { Ellipse } from './Ellipse.js'
+import { Circle } from './Circle.js'
+
+// TODO: Instantialize object Circle.
+// TODO: Instantialize object Ellipse.
