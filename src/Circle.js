7985d67dfbfea5a613bdbe61b38f032656c22600
Pauliina Raitaniemi <pr222ja@student.lnu.se>
2020-10-15 10:36:28 +0200

    Add empty Circle class

---
+++ src/Circle.js
@@ -0,0 +1,24 @@
+/**
+ * Circle module.
+ *
+ * @module src/Circle
+ * @author Pauliina Raitaniemi <pr222ja@student.lnu.se>
+ * @version 1.0.0
+ */
+
+// ------------------------------------------------------------------------------
+//  Public interface
+// ------------------------------------------------------------------------------
+import { Ellipse } from './Ellipse.js'
+
+/**
+ * Represents a circle.
+ *
+ * @class Circle
+ * @extends {Ellipse} - Inherits from Ellipse class.
+ */
+export class Circle extends Ellipse {
+    constructor () {
+        
+    }
+}
